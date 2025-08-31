488659bd5391d2ccc20f2cf797c7214dc8ce47b8
Dhanashree <dhanashrees138@gmail.com>
2025-08-31 15:23:15 +0530

    removing the deprecated stuff that is failling the deploy on render

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use('/api/blood', bloodRoutes);
 app.use('/api/users', userRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('✅ MongoDB connected');
         app.listen(process.env.PORT || 5000, () => {
