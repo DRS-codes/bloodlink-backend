85c268502745692f044699afb6b87a611082fbb2
DRS-codes <dhanashrees138@gmail.com>
2026-03-07 19:04:32 +0530

    cors links

--- index.js
+++ index.js
@@ -11,7 +11,17 @@ const bloodRequestRoutes = require('./routes/bloodRequestRoutes');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: [
+            "http://127.0.0.1:5500",
+            "http://localhost:5500",
+            "https://drs-codes.github.io/bloodlink-f/bloodlink.html"
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 // Routes
