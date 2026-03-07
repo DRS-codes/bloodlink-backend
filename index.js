c4712599ae2354113dc32d9137e8b742d9867fbc
DRS-codes <dhanashrees138@gmail.com>
2026-03-07 19:04:52 +0530

    cors links

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors(
         origin: [
             "http://127.0.0.1:5500",
             "http://localhost:5500",
-            "https://drs-codes.github.io/bloodlink-f/bloodlink.html"
+            "https://drs-codes.github.io"
         ],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
